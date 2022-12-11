673a89cdd153dd68ce9f23396c204b79675d9eeb
Andres Uru <98914498+Uru198@users.noreply.github.com>
2022-12-11 18:49:42 -0500

    css adelanto

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import 'animate.css';
+import { useHistory } from "react-router-dom";
+import axios from "axios"
 import AllCakes from "./componentes/AllCakes/AllCakes";
 import Cake from "./componentes/Cake/Cake";
 import NewCake from "./componentes/NewCake/NewCake";
 import styles from "./App.module.css";
 import UpdateCake from "./componentes/UpdateCake/UpdateCake";
-import Registro from "./componentes/Login/Registro";
-import Login from "./componentes/Login/Login";
+import Registro from "./componentes/Registro";
+import Login from "./componentes/Login";
 import Cupcakes from "./componentes/AllCakes/Cupcakes";
 import Galletas from "./componentes/AllCakes/Galletas";
 import Genovesas from "./componentes/AllCakes/Genovesas";
@@ -15,58 +18,91 @@ import OtrosProductos from "./componentes/AllCakes/OtrosProductos";
 import Addimage from "./componentes/Addimage";
 import Cart from "./componentes/AllCakes/Cart";
 import Inicio from "./componentes/Inicio";
+import InicioAbierto from "./componentes/InicioAbierto";
+import Cookies from 'universal-cookie';
+import { useEffect } from "react";
 
 function App() {
 
+  const cookies = new Cookies();
+  const tipoUsuario = null ?? cookies.get('rol');
 
+  useEffect(() => {
+    const tipoUsuario = null ?? cookies.get('rol');
+
+  },[]
+  )
+
+
+  const cerrarSesion = () => {
+    axios.get('http://localhost:8000/api/logout')
+        .then(res => {
+         cookies.remove("rol")
+
+          history.push('/')
+        } )
+        .catch(err => console.log(err));
+}
+
+const history = useHistory();
   return (
     <div className={`${styles.cont1}`}>
 
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-around py-3 mb-4 border-bottom fixed-top">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-      </a>
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-around py-3 mb-4 border-bottom fixed-top">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+        </a>
 
-      <ul class="d-flex align-items-center nav col-13 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Inicio</a></li>
-        <li className={`${styles.li1}`}><a href="#" class="nav-link px-2 link-dark"> <div class="dropdown">
-            <button className={`${styles.btnHover1} btn dropdown-toggle`}type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Productos 
+        <ul class="d-flex align-items-center nav col-13 col-md-auto mb-2 justify-content-center mb-md-0">
+
+          <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link link-dark`}>Inicio</a></li>
+          <li className={`${styles.li1}`}><a href="#" class="nav-link px-2 link-dark"> <div class="dropdown">
+            <button className={`${styles.btnHover1} btn dropdown-toggle`} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Productos
             </button>
             <ul class={`${styles.dropdown} dropdown-menu`}>
-              <li ><a class={`${styles.dropdownMenu} dropdown-item `}href="#">Cupcakes</a></li>
+              <li ><a class={`${styles.dropdownMenu} dropdown-item `} href="/">Cupcakes</a></li>
               <li><a class={`${styles.dropdownMenu} dropdown-item `} href="#">Galletas</a></li>
               <li><a class={`${styles.dropdownMenu} dropdown-item `} href="#">Postres</a></li>
               <li><a class={`${styles.dropdownMenu} dropdown-item `} href="#">Tortas</a></li>
             </ul>
           </div></a></li>
-        <li className={`${styles.li1}`}><a href="#" class="">
-          <div className={`${styles.imgLogo}`}>
-                <img src="/imagenes/logo.jpeg" alt="logo" className= {`${styles.img} col-3 img-fluid`}/> 
+
+
+          <li className={`${styles.li1}`}><a href="#" class="">
+            <div className={`${styles.imgLogo}`}>
+              <img src="/imagenes/logo.jpeg" alt="logo" className={`${styles.img} col-3 img-fluid`} />
+            </div>
+          </a></li>
+
+          <div className="d-flex flex-row">
+            <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Quienes Somos</a></li>
+            <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Contactanos</a></li>
+
           </div>
-        </a></li>
-        <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Quienes Somos</a></li>
-        <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Contactanos</a></li>
-      </ul>
+        </ul>
 
-      <div class="col-md-3 text-end">
-        <a  href="/login"><button type="button" class="btn btn-outline-primary me-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Login
-        </button></a>
-        
-        <a href="/registro"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
-          Register
-        </button></a>
-        
+        <div class="col-md-3 text-end">
+          {tipoUsuario == null? (
+            <div>      <a href="/login"><button type="button" class={`${styles.btnL} btn  me-2`} data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Inicia Sesion
+          </button></a>
+            <a href="/registro"><button type="button" class={`${styles.btnR} btn `} data-bs-toggle="modal" data-bs-target="#exampleModal1">
+              Registrate
+            </button></a>  </div>
+           
+
+          ) : (<div> 
+            <div>
+              <button className={`${styles.btn2} link-light`} onClick={cerrarSesion}>Cerrar Sesión</button>
+          </div> 
+          </div>)}
+
         </div>
-       
 
-      
-    </header>
-   
- 
 
-      <br/>
+      </header>
+      <br />
       <BrowserRouter>
         <Switch>
           <Route path="/registro" exact render={() => <Registro />} />
@@ -81,9 +117,12 @@ function App() {
           <Route path="/otros_productos" exact render={() => <OtrosProductos />} />
           <Route path="/cake/new" exact render={() => <NewCake />} />
           <Route path="/cake/:id" exact render={(routeProps) => <Cake {...routeProps} />} />
-          <Route path="/cake/update/:id" render={() => <UpdateCake />}/>
-          <Route path="/imagen" render={() => <Addimage/>}/>
+          <Route path="/cake/update/:id" render={() => <UpdateCake />} />
+          <Route path="/imagen" render={() => <Addimage />} />
           <Route path="/cart" render={() => <Cart />} />
+          <Route path="/InicioAbierto" render={() => <InicioAbierto />} />
+
+
         </Switch>
       </BrowserRouter>
     </div>
