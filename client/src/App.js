910b05ef2a615bb8f7f2a32985126003fa38a31a
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-12-12 20:39:44 -0500

    Fixed what's app button

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import Inicio from "./componentes/Inicio";
 import InicioAbierto from "./componentes/InicioAbierto";
 import Cookies from 'universal-cookie';
 import { useEffect } from "react";
+import Contactanos from "./componentes/Contactanos";
 
 function App() {
 
@@ -37,7 +38,7 @@ function App() {
   const cerrarSesion = () => {
     axios.get('http://localhost:8000/api/logout')
         .then(res => {
-         cookies.remove("rol")
+          cookies.remove("rol")
 
           history.push('/')
         } )
@@ -77,7 +78,7 @@ const history = useHistory();
 
           <div className="d-flex flex-row">
             <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Quienes Somos</a></li>
-            <li className={`${styles.li1}`}><a href="#" className={`${styles.btnHover} nav-link px-2 link-dark`}>Contactanos</a></li>
+            <li className={`${styles.li1}`}><a href="/contactenos" className={`${styles.btnHover} nav-link px-2 link-dark`}>Contactanos</a></li>
 
           </div>
         </ul>
@@ -121,6 +122,7 @@ const history = useHistory();
           <Route path="/imagen" render={() => <Addimage />} />
           <Route path="/cart" render={() => <Cart />} />
           <Route path="/InicioAbierto" render={() => <InicioAbierto />} />
+          <Route path="/contactanos" render={() => <Contactanos />} />
 
 
         </Switch>
---
+++ client/src/componentes/Contactanos.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../App.module.css";
+
+
+const Contactanos = () => {
+
+    return (
+        <div>
+            <p>tesasdasdsat</p>
+
+        </div>
+
+    )
+
+}
+export default Contactanos;
